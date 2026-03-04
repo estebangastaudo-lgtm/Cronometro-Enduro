3185edc8b9908ad1fda1a9282664537e19c51599
estebangastaudo-lgtm <estebangastaudo@gmail.com>
2026-03-04 09:55:18 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE = "cronometro-equipo-v2-1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon.svg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k === CACHE ? null : caches.delete(k))))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  event.respondWith(
+    caches.match(req).then((cached) => cached || fetch(req).then((resp) => {
+      // Cache new same-origin GET responses
+      try {
+        const url = new URL(req.url);
+        if (req.method === "GET" && url.origin === location.origin) {
+          const copy = resp.clone();
+          caches.open(CACHE).then((cache) => cache.put(req, copy));
+        }
+      } catch (e) {}
+      return resp;
+    }).catch(() => caches.match("./index.html")))
+  );
+});
